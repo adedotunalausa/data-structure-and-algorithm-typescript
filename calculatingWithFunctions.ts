e6bcfce54eeac0c1589c986f48eeeff91cac3a51
Adedotun Alausa <adedotunalausa@gmail.com>
2021-12-13 09:34:59 +0100

    add calculating with functions

--- calculatingWithFunctions.ts
+++ calculatingWithFunctions.ts
@@ -1,75 +1,20 @@
-function zero(arg?: any) {
-  if (arg) {
-    return Math.floor(eval(0 + arg));
-  }
-  return 0;
-}
-function one(arg?) {
-  if (arg) {
-    return Math.floor(eval(1 + arg));
-  }
-  return 1;
-}
-function two(arg?) {
-  if (arg) {
-    return Math.floor(eval(2 + arg));
-  }
-  return 2;
-}
-function three(arg?) {
-  if (arg) {
-    return Math.floor(eval(3 + arg));
-  }
-  return 3;
-}
-function four(arg?) {
-  if (arg) {
-    return Math.floor(eval(4 + arg));
-  }
-  return 4;
-}
-function five(arg?) {
-  if (arg) {
-    return Math.floor(eval(5 + arg));
-  }
-  return 5;
-}
-function six(arg?) {
-  if (arg) {
-    return Math.floor(eval(6 + arg));
-  }
-  return 6;
-}
-function seven(arg?) {
-  if (arg) {
-    return Math.floor(eval(7 + arg));
-  }
-  return 7;
-}
-function eight(arg?) {
-  if (arg) {
-    return Math.floor(eval(8 + arg));
-  }
-  return 8;
-}
-function nine(arg?) {
-  if (arg) {
-    return Math.floor(eval(9 + arg));
-  }
-  return 9;
-}
+const numberFunction = (number: number) => (operatorFunction?: Function) => {
+  return operatorFunction ? operatorFunction(number) : number;
+};
+const zero = numberFunction(0);
+const one = numberFunction(1);
+const two = numberFunction(2);
+const three = numberFunction(3);
+const four = numberFunction(4);
+const five = numberFunction(5);
+const six = numberFunction(6);
+const seven = numberFunction(7);
+const eight = numberFunction(8);
+const nine = numberFunction(9);
 
-function plus(arg) {
-  return `+${arg}`;
-}
-function minus(arg) {
-  return `-${arg}`;
-}
-function times(arg) {
-  return `*${arg}`;
-}
-function dividedBy(arg) {
-  return `/${arg}`;
-}
+const plus = (r?: number) => (l?: number) => l + r;
+const minus = (r?: number) => (l?: number) => l - r;
+const times = (r?: number) => (l?: number) => l * r;
+const dividedBy = (r?: number) => (l?: number) => l / r;
 
-console.log(nine(dividedBy(two())));
+console.log(nine(times(two())));
